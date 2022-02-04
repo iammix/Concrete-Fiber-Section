c370589be0ef664709c0a7928a8d756856dbf52f
Konstantinos Mixios <k.mixios@gmail.com>
2022-02-04 11:50:51 +0200

    UPDATE

--- fiberSection/fiber.py
+++ fiberSection/fiber.py
@@ -6,3 +6,16 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import pandas
 import os
+
+
+class Fiber:
+    def __init__(self, xx1, xx2, yy1, yy2, material1, material2):
+        self.xx1 = xx1
+        self.xx2 = xx2
+        self.yy1 = yy1
+        self.yy2 = yy2
+        self.material1 = material1
+        self.material2 = material2
+        self.position_x = []
+        self.position_y = []
+
